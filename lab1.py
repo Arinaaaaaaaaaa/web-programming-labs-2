fcd3c77c9047d8dbd6f637d912b18f953662c227
Арина Малкова <arinamalkova.200306@gmail.com>
2023-10-14 10:54:41 +0700

    Заготовка к третьей лабораторной работе

--- lab1.py
+++ lab1.py
@@ -31,6 +31,10 @@ def menu():
                 <li>
                     <a href="/lab2/",target="_blank">Вторая лабораторная работа</a>
                 </li>
+
+                <li>
+                    <a href="/lab3/",target="_blank">Третья лабораторная работа</a>
+                </li>
             </ol>
         </main>
 
