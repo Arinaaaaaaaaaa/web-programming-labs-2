8eb741509d787acd5f32f93d9acc7f9b37ab1ab7
Арина Малкова <arinamalkova.200306@gmail.com>
2023-12-06 20:27:33 +0700

    Страница регистрации

--- lab6.py
+++ lab6.py
@@ -30,3 +30,42 @@ def mainart():
     return "Result in console!"
 
 
+@lab6.route("/lab6/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register_6.html")
+    
+    username_form = request.form.get("username")
+    password_form = request.form.get("password")
+
+    #Проверяем существование пользователя в БД с таким же именем 
+    #Если такого пользователя нет, то в isUserExist вернется None, т.e. мы можем интерпретировать это как False
+
+    #select * from users
+    #WHERE username = username_form
+    #LIMIT 1
+    #где username_form - это имя, которое мы получили из формы
+    isUserExist = users.query.filter_by(username = username_form).first()
+    errors = []
+    if isUserExist is not None:
+        errors.append("Такой пользователь уже существует!")
+        return render_template("register_6.html", errors = errors)
+    elif not username_form:
+        errors.append("Введите имя пользователя!")
+        return render_template("register_6.html", errors=errors)
+    elif len(password_form) < 5:
+        errors.append("Пароль должен содержать не менее 5 символов!")
+        return render_template("register_6.html", errors=errors)
+
+    #Хэшируем пароль
+    hashedPswd = generate_password_hash(password_form, method="pbkdf2")
+    #Создаем объект users с нужными полями
+    newUser = users(username = username_form, password = hashedPswd)
+
+    #Это INSERT
+    db.session.add(newUser)
+    #Тоже самое, что и conn.commit()
+    db.session.commit()
+
+    #Перенаправляем на страницу логина
+    return redirect("/lab6/login")
