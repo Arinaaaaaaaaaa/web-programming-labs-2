d9f569e0f0b66be5a0783df8aac8ddefecb3e462
Арина Малкова <arinamalkova.200306@gmail.com>
2023-12-07 23:42:26 +0700

    Вывод статей пользователей

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
-
+from lab6 import lab6
 
 from flask_sqlalchemy import SQLAlchemy
-from lab6 import lab6
 from Db import db
 from Db.models import users
 from flask_login import LoginManager
@@ -41,10 +40,10 @@ def load_users(user_id):
     #Метод get вернет объект users с нужным id со всеми атрибутами и методами класса
     return users.query.get(int(user_id))
 
-app.register_blueprint(lab6)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
--- lab6.py
+++ lab6.py
@@ -110,3 +110,49 @@ def login():
             return redirect("/lab6/articles")
 
     return render_template("login_6.html")
+
+
+#login_required - авторизация обязательна, если пользователь не авторизован, то перенаправить
+#на страницу lab6/login (lab6/login мы указывали в app.pу)
+
+#Функцию login_required и переменнyю current_user мы импортировали ранее из Flask-Login
+@lab6.route('/lab6/articles', methods = ['GET', 'POST'])
+@login_required
+def articles_list():
+    username_form = request.form.get('username')
+    my_articles = articles.query.filter((articles.user_id == current_user.id) | (articles.is_public == True)).order_by(articles.is_favorite.desc()).all()
+    print(my_articles)
+    return render_template('articles_6.html', articles=my_articles, username_form = username_form)
+
+
+@lab6.route("/lab6/newarticle", methods=["GET", "POST"])
+@login_required
+def createArticle():
+    if request.method == "GET":
+        return render_template("new_article_6.html")
+    if request.method == "POST":
+        article_text = request.form.get("article_text")
+        title = request.form.get("article_title")
+        if len(article_text) == 0 or title is None:
+            errors = ["Заполните текст и заголовок"]
+            return render_template("new_article_6.html", errors=errors)
+        new_article = articles(user_id=current_user.id, title=title, article_text=article_text)
+
+        db.session.add(new_article)
+        db.session.commit()
+
+        return redirect("/lab6/articles")
+
+
+@lab6.route("/lab6/articles/<int:article_id>", methods=['GET', 'POST'])
+def getArticle(article_id):
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            article = articles.query.filter_by(id=article_id).first()
+        article = articles.query.filter_by(id=article_id).first()
+        if article:
+            if article.user_id == current_user.id or article.is_public:
+                text = article.article_text.splitlines()
+                return render_template("articlescheck_6.html", article_text=text, article_title=article.title, username=current_user.username)
+
+    return redirect("/lab6/articles")
