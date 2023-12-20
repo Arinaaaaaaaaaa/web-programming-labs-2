a0c55807b50cde2ff9e910892980d8043e0f96bc
Зенкова Алина <zenkovaalinam@gmail.com>
2023-12-20 14:41:01 +0700

    Добавление нового курса по REST

--- lab8.py
+++ lab8.py
@@ -39,4 +39,10 @@ def put_course(course_num):
 
     else:
         return courses[course_num]
-   
+
+@lab8.route('/lab8/api/courses/', methods = ['POST'])
+def add_course():
+    course = request.get_json()
+    courses.append(course)
+    return {"num": len(courses)-1}
+
