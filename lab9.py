bd3f516a6278e962ac046794df63f38d0040f961
Зенкова Алина <zenkovaalinam@gmail.com>
2023-12-21 09:39:45 +0700

    Обработчик ошибки 404

--- lab9.py
+++ lab9.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 lab9 = Blueprint('lab9', __name__)
 
 @lab9.route('/lab9/')
 def main():
     return render_template('lab9/index.html')
+
+@lab9.app_errorhandler(404)
+def not_found(e):
+    return render_template('lab9/404.html'), 404
+ 
+    
